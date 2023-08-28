593f1c4c696e08d5ab1e9dc81df947d2e06f6739
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-08-29 02:06:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+document.addEventListener("DOMContentLoaded", function() {
+      var openModalButton = document.getElementById("open-modal");
+      var newtonModal = new bootstrap.Modal(document.getElementById("newton-modal"));
+
+      openModalButton.addEventListener("click", function() {
+        newtonModal.show();
+      });
+    });
